68a93a71f3dc18d941175cf0838abd606dc53f67
vistej <teja.pvt@gmail.com>
2024-03-30 01:13:23 +0000

    Code challenge submission for question: 992 Subarrays with K Different Integers

---
+++ 992 Subarrays with K Different Integers/992subarrays-with-k-different-integers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
+        counter = {}
+        pre_cnt = 0
+        l = 0
+        res = 0
+        for r in range(len(A)):
+            counter[A[r]] = counter.get(A[r], 0) + 1
+            if len(counter) > K:
+                counter.pop(A[l])
+                l += 1
+                pre_cnt = 0
+            
+            while counter[A[l]] > 1:
+                counter[A[l]] -= 1
+                pre_cnt += 1
+                l += 1
+            
+            if len(counter) == K:
+                res += pre_cnt + 1
+        
+        return res
