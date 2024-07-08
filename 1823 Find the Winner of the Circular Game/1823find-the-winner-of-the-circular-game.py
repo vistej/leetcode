824890321384ee440341ad072b0762ffdb461b9d
vistej <teja.pvt@gmail.com>
2024-07-08 19:20:01 +0000

    Code challenge submission for question: 1823 Find the Winner of the Circular Game

---
+++ 1823 Find the Winner of the Circular Game/1823find-the-winner-of-the-circular-game.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        winner = 0
+        for i in range(1, n + 1):
+            winner = (k + winner) % i
+        return winner + 1
