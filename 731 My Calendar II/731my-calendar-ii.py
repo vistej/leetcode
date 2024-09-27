a4a0e5acde2f187e33c3a8a3bf6b64ae9231b0bd
vistej <teja.pvt@gmail.com>
2024-09-27 23:22:41 +0000

    Code challenge submission for question: 731 My Calendar II

---
+++ 731 My Calendar II/731my-calendar-ii.py
@@ -0,0 +1,16 @@
+class MyCalendarTwo:
+    def __init__(self):
+        self.mp = defaultdict(int)
+    def book(self, start: int, end: int) -> bool:
+        self.mp[start] += 1
+        self.mp[end] -= 1
+
+        ActiveCount = 0
+        for value, count in sorted(self.mp.items()):
+            ActiveCount += count
+            if ActiveCount > 2:
+                print('asdf')
+                self.mp[start] -= 1
+                self.mp[end] += 1
+                return False
+        return True
