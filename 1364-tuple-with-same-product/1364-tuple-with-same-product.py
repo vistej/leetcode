43adf722e785503bbb900185898547f1a9d51c04
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-02-06 13:08:13 -0500

    1364-TUPLE-WITH-SAME-PRODUCT Stats: Time: 579 ms (14.69%), Space: 46.3 MB (65.4%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,18 +1,13 @@
 class Solution:
-    def tupleSameProduct(self, nums: List[int]) -> int:
-        if len(nums) < 4:
-            return 0
-        prod_keep = defaultdict(list)
+  def tupleSameProduct(self, nums: list[int]) -> int:
+    
+    ans = 0
+    count = collections.Counter()
 
-        res = 0
+    for i in range(len(nums)):
+      for j in range(i):
+        prod = nums[i] * nums[j]
+        ans += count[prod] * 8
+        count[prod] += 1
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                p = nums[i] * nums[j]
-                if prod_keep[p]:
-                    for tup in prod_keep[p]:
-                        if nums[i] not in tup and nums[j] not in tup:
-                            res += 8
-                prod_keep[p].append((nums[i], nums[j]))
-        return res
-
+    return ans
