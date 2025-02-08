ffe0d55e792972878d1e82d9c1ca6c8fa14be207
Vishnu Paturu <42509698+vistej@users.noreply.github.com>
2025-02-07 21:07:38 -0500

    0401-BINARY-WATCH Stats: Time: 3 ms (32.59%), Space: 17.8 MB (41.34%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,29 +1,13 @@
 class Solution:
-    def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        hs = [0, 1, 2, 4, 8]
-        ms = [0, 1, 2, 4, 8, 16, 32]
-        res = set()
+    def readBinaryWatch(self, num: int) -> list[str]:
+        times = []
 
-        def dfs(i, j, h, m):
-            sh = sum(h)
-            sm = sum(m)
-            if sh > 11 or sm > 59 or len(h) + len(m) > turnedOn:
-                return
-            if len(h) + len(m) == turnedOn:
-                nm = str(sm) if sm > 9 else '0' + str(sm)
-                res.add(str(sh) + ':' + nm)
-                return
-            if i + 1 < len(hs):
-                dfs(i + 1, j, h, m)
-                h.append(hs[i + 1])
-                dfs(i + 1, j, h, m)
-                h.pop()
-            if j + 1 < len(ms):
-                dfs(i, j + 1, h, m)
-                m.append(ms[j + 1])
-                dfs(i, j + 1, h, m)
-                m.pop()
-        
-        dfs(0, 0, [], [])
+        for h in range(12):
+            for m in range(60):
+                hOnes = bin(h).count('1')
+                mOnes = bin(m).count('1')
 
-        return list(res)
+                if hOnes + mOnes == num:
+                    times.append(f"{h}:{m:02d}")
+
+        return times
